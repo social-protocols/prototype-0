1c31cf67b7c01d55d35009603ce2743d668fe592
JohannesNakayama <johannes.nakayama@rwth-aachen.de>
2023-09-15 17:31:21 +0200

    feat: sketch data model

--- migrations/20230913130433_create-post-table.sql
+++ migrations/20230913130433_create-post-table.sql
@@ -2,5 +2,21 @@ create table post (
     id integer primary key -- row id
     , content text not null
     , parent integer references post (id) on delete cascade on update cascade -- nullable
+    , question_id integer post (id) on delete cascade on update cascade -- nullable
+        -- if a question is a top-level question -> no parent and question_id = id
+        -- if a question is a reply -> parent is the id of the post it replies to
+        -- -- if it reuses a question -> that question is its question_id
+        -- -- if it doesn't reuse a question -> question_id = id
+);
+
+create table vote_history (
+    post_id not null references post (id) on delete cascade on update cascade
+    , user_id not null references user (id) on delete cascade on update cascade
+    , timestamp timestamp without timezone
+    , direction integer not null
+);
+
+create table user (
+    id primary key
 );
 
