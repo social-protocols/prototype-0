5a5ca14b6d07c711945170b2db222b94ad0f568b
Felix Dietze <felix.dietze@rwth-aachen.de>
2023-09-15 12:23:46 +0200

    fix query

--- src/persona.rs
+++ src/persona.rs
@@ -1,5 +1,6 @@
-use crate::SqlitePool;
+use anyhow::Result;
 use serde::Serialize;
+use sqlx::FromRow;
 use sqlx::SqlitePool;
 
 /*
@@ -12,7 +13,7 @@ create table persona (
 
 // Declare persona type corresponding to persona table above
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Debug, FromRow)]
 pub struct Persona {
     id: i64,
     description: String,
@@ -20,12 +21,11 @@ pub struct Persona {
 
 pub async fn get_personas(sqlite_pool: SqlitePool) -> Result<Vec<Persona>> {
     // use sqlx to return a vec<Result> directly from the database
-    let personas: Vec<Persona> = sqlx::query_as!(
-        Persona,
+    let personas: Vec<Persona> = sqlx::query_as::<_, Persona>(
         r#"
             select id, description
             from persona
-        "#
+        "#,
     )
     .fetch_all(&sqlite_pool)
     .await?;
